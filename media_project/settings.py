4de49bd9b8c61846a3cc658c6628eea66f74b958
OIITuMuCT <vataman@live.ru>
2024-10-17 20:26:51 +0300

    /media/test.txt

--- media_project/settings.py
+++ media_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "media_example",
+    # "media_example",
 ]
 
 MIDDLEWARE = [
@@ -118,12 +118,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-MEDIA_ROOT = BASE_DIR / "media"
-
-MEDIA_URL = "/media/"
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
