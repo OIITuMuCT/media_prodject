5fe9b77afccce8c7dc21016a7bc0aa98c8768c98
OIITuMuCT <vataman@live.ru>
2024-10-17 20:36:40 +0300

    start exercise 8.02

--- media_example/views.py
+++ media_example/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.conf import settings
+from myproject.myapp.models import Review
 
 # Create your views here.
+def my_view(request):
+    return render(request, "template.html", {"MEDIA_URL":
+        settings.MEDIA_URL, "username": "jbloggs"})
+
+def media(request):
+    """
+    Add media-related context variables to the context.
+    """
+    return {"MEDIA_URL": settings.MEDIA_URL}
+
+
+def latest_review(request):
+    return {"latest_reviews": Review.objects.order_by("-date_created")[:5]}
